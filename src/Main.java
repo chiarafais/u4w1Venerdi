c9e9aeab2e6ed3a257d308fa04e8f11e2a436754
chiarafais <chiara0915@gmail.com>
2024-05-24 16:59:06 +0200

    the end

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import entities.Audio;
 import entities.Image;
+import entities.MultimedialElement;
 import entities.Video;
 import enums.TypeElement;
 
@@ -7,6 +8,14 @@ import java.util.Scanner;
 
 public class Main {
    public static void main(String[] args) {
+
+       String title;
+       int volume;
+       int length; //durata audio/video
+       int brightness;
+       TypeElement typoElement;
+
+
 //        System.out.println("-------------------AUDIO--------------------");
 //        Audio audio = new Audio("prova canzone",5,3, TypeElement.AUDIO);
 //        audio.play();
@@ -17,9 +26,75 @@ public class Main {
 //        Image image = new Image("prova immagine", 5, TypeElement.IMAGE);
 //        image.show();
 
+
+       MultimedialElement[] arrayFive = new MultimedialElement[5];
        Scanner sc = new Scanner(System.in);
-       System.out.println("");
-       String text = sc.nextLine();
+       System.out.println("scegli 5 file multimediali: ");
+
+
+       for (int i=0; i< arrayFive.length; i++){
+           System.out.println("quale file scegli: AUDIO, VIDEO, IMAGE ?");
+           String text = sc.nextLine();
+           TypeElement tipo = TypeElement.valueOf(text);
+
+           switch(tipo) {
+               case AUDIO: {
+                   System.out.println("hai scelto AUDIO, ora inserisci un titolo: ");
+                   title = sc.nextLine();
+                   System.out.println("dimmi la durata : ");
+                   length = Integer.parseInt(sc.nextLine());
+                   System.out.println("il volume da 0 a 10 : ");
+                   volume = Integer.parseInt(sc.nextLine());
+                   arrayFive[i] = new Audio (title,length,volume,tipo);
+                   break;
+               }
+               case VIDEO : {
+                   System.out.println("hai scelto VIDEO, ora inserisci un titolo: ");
+                   title = sc.nextLine();
+                   System.out.println("dimmi la durata : ");
+                   length = Integer.parseInt(sc.nextLine());
+                   System.out.println("il volume da 0 a 10 : ");
+                   volume = Integer.parseInt(sc.nextLine());
+                   System.out.println("la luminosità da 0 a 10 : ");
+                   brightness = Integer.parseInt(sc.nextLine());
+                   arrayFive[i] = new Video (title,length,volume,brightness,tipo);
+                   break;
+               }
+               case IMAGE:{
+                   System.out.println("hai scelto IMAGE, ora inserisci un titolo: ");
+                   title = sc.nextLine();
+                   System.out.println("la luminosità da 0 a 10 : ");
+                   brightness = Integer.parseInt(sc.nextLine());
+                   arrayFive[i] = new Image (title,brightness,tipo);
+                   break;
+               }
+               default:
+                   System.out.println("errore");
+                   break;
+           }
+
+
+       }
+       while (true){
+           System.out.println("inserisci un numero da 1 a 5 per scegliere uno degli elementi che hai inserito!");
+           int numSelect = Integer.parseInt(sc.nextLine());
+           if(numSelect == 0){
+               break;
+           }if (numSelect >= 1 && numSelect <= 5){
+               MultimedialElement element = arrayFive[numSelect - 1];
+               if(element instanceof Image){
+                   ((Image) element).show();
+               }
+               if(element instanceof Audio){
+                   ((Audio) element).play();
+               }
+               if(element instanceof Video){
+                   ((Video) element).play();
+               }
+
+           }
+
+       }
 
     }
 }
